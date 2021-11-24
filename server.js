b400045eacf87b8df2f0733d0109b67d32031438
Jake <xxjakexx14@gmail.com>
2021-11-24 18:40:17 -0500

    added handlebar partials for post and comments with helpers

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const exphbs = require("express-handlebars");
 
-//TODO add helpers
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 const path = require("path");
 
